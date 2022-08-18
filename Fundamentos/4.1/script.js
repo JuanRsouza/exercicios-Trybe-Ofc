3c46348f8db5089f4a279c59280d84240706d9d0
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-08-18 18:47:06 -0300

    Alterações em arquivos

---
+++ Fundamentos/4.1/script.js
@@ -0,0 +1,16 @@
+let Candidato = "Fábio";
+
+switch (Candidato) {
+  case "André" :
+    console.log("Lista");
+    break;
+  case "Juan" :
+    console.log("Aprovado");
+    break;
+    case "Lucas" :
+      console.log("Reprovado");
+      break;
+    default: 
+    console.log("Não foi dessa vez");
+    
+}
