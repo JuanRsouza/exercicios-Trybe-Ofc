0383e8c3a494998cfd76378f391977514b5507a2
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-08-19 20:37:44 -0300

    Encerrando o dia

--- Fundamentos/4.2/script.js
+++ Fundamentos/4.2/script.js
@@ -1,9 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
+let media = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
     resultado += numbers[index];
 }
+ media = resultado / numbers.length;
 
-console.log(resultado);
+ if (media >= 20) {
+    console.log("valor maior que 20")
+ } else {
+    console.log("valor menor ou igual a 20")
+ }
+
+
+
 
