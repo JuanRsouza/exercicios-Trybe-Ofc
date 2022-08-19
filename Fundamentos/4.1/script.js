076070f42a8c8d1795d2a9d19942fa3486b0a2f1
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-08-19 17:58:39 -0300

    Terminei exercicios 4.1

--- Fundamentos/4.1/script.js
+++ Fundamentos/4.1/script.js
@@ -1,16 +1,1 @@
-let Candidato = "Rodrigo";
-
-switch (Candidato) {
-  case "André" :
-    console.log("Lista");
-    break;
-  case "Juan" :
-    console.log("Aprovado");
-    break;
-    case "Lucas" :
-      console.log("Reprovado");
-      break;
-    default: 
-    console.log("Não foi dessa vez");
-    
-}
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
