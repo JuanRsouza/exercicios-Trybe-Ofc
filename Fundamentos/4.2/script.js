0dc22c965ef1edd9386279689cfc6ae1bfe5137f
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-08-22 18:33:32 -0300

    Finalizando exercicios

--- Fundamentos/4.2/script.js
+++ Fundamentos/4.2/script.js
@@ -62,4 +62,14 @@
 
 // Exercício 9 finalizado
 
+//Crie um algoritmo que recebe a idade de Noel, Ellen e Nasc e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbersMulti = [];
+
+for(let index = 0; index < numbers.length; index += 1) {
+    numbersMulti.push += numbers[index]
+    
+}
+console.log(numbersMulti);
