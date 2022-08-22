f9a7548a469b828b4a75348c7c4dd8c435250a71
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-08-22 12:08:30 -0300

    Exercicios finalizados

--- Fundamentos/4.2/script.js
+++ Fundamentos/4.2/script.js
@@ -1,18 +1,65 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-let media = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+// let media = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index];
-}
- media = resultado / numbers.length;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   resultado += numbers[index];
+// }
+// media = resultado / numbers.length;
 
- if (media >= 20) {
-    console.log("valor maior que 20")
- } else {
-    console.log("valor menor ou igual a 20")
- }
+// if (media >= 20) {
+//   console.log("valor maior que 20")
+// } else {
+//   console.log("valor menor ou igual a 20")
+// }
 
+// exercício 4 finalizado !
 
+// ----------------------------
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let even = [];
+// let odd = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 === 0) {
+//     even += numbers[index];
+//   } else {
+//     odd += numbers[index];
+//   }
+  
+// }
+// console.log (odd, even);
+
+// ---------------------------------
+
+// let array = [];
+
+// for (let index = 1; index <= 25; index += 1) {
+//   array.push(index);
+     
+//   }
+
+// console.log (array);
+
+// Exercício 8 finalizado
+
+// --------------------------
+
+// let array = [];
+// let divisao = [];
+
+// for (let index = 1; index <= 25; index += 1) {
+//   array.push(index);
+     
+//   }
+
+// for (let index = 0; index < array.length; index += 1) {
+// divisao.push(array[index] / 2);
+// }
+// console.log(array);
+// console.log(divisao);
+
+// Exercício 9 finalizado
 
 
