1e273f0b7d6a11ce0ba18bb09d386b855de3be93
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-09-06 22:18:38 -0300

    Terminei meu formulário

---
+++ Fundamentos/6.1/script.js
@@ -0,0 +1,18 @@
+// SELETORES
+
+// const botaoEnviar = document.querySelector("#button-enviar");
+
+// function button (event) {
+//     event.preventDefault()
+// }
+// botaoEnviar.addEventListener('click', button)
+
+// function text (event) {
+//     const key = event.key;
+//     if(key !== 'a') {
+//     event.preventDefault();
+//     }
+// }
+// INPUT_TEXT.addEventListener('keypress', text);
+
+// Captura um evento de tecla
